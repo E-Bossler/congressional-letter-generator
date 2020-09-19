25935d80255bb69a15b053e811b3898a96fd2e60
Eric Bossler <eric.bossler@gmail.com>
2020-09-19 15:27:52 -0700

    recipient list pulling correct information

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,48 @@
 const reciptientOptions = document.getElementById('recipients')
-
-const congressNumber=116
-
+const generateButton = document.getElementById('generate')
+const senderName = document.getElementById('senderName')
+const senderAddress = document.getElementById('senderAddress')
+const senderPhone = document.getElementById('senderPhone')
+const senderEmail = document.getElementById('email_input')
+const senderMessage = document.getElementById('message_area')
+const congressNumber = 116
 const apiKeyHeader = {
     "X-API-Key": "1ZTW2IDKkCIQocFGrH0USEmDZSiY7yUG4VYJ31Rz"
 }
-
-const recipients = []
+const recipients = [
+    {
+        title: "Select Recipients",
+        firstName: "",
+        lastName: "",
+        state: "",
+        inOffice: true
+    },
+    {
+        title: "All Senators",
+        firstName: "and",
+        lastName: "House Members",
+        state: "",
+        inOffice: true
+    },
+    {
+        title: "",
+        firstName: "All",
+        lastName: "Senators",
+        state: "",
+        inOffice: true
+    },
+    {
+        title: "",
+        firstName: "All",
+        lastName: "House Members",
+        state: "",
+        inOffice: true
+    }
+]
+const senators = []
+const houseMembers = []
+const allCongressPeople = []
+const singleMember = []
 
 function getSenateMembers() {
     fetch(
@@ -20,51 +56,180 @@ function getSenateMembers() {
     ).then(
         json => {
             const memberArray = json.results[0].members
-            for (let i=0; i<memberArray.length; i++) {
-                recipients.push(
-                    {
+            for (let i = 0; i < memberArray.length; i++) {
+                if (memberArray[i].in_office === true) {
+                    recipients.push(
+                        {
+                            "title": memberArray[i].short_title,
+                            "firstName": memberArray[i].first_name,
+                            "lastName": memberArray[i].last_name,
+                            "address": memberArray[i].office,
+                            "addressSecondLine": "Washington, DC 20510",
+                            "inOffice": memberArray[i].in_office,
+                            "state": memberArray[i].state,
+                            "phone": memberArray[i].phone,
+                            "fax": memberArray[i].fax,
+                            "site": memberArray[i].url, 
+                            "email": memberArray[i].contact_form
+                        }
+                    )
+
+                    senators.push(
+                        {
+                            "title": memberArray[i].short_title,
+                            "firstName": memberArray[i].first_name,
+                            "lastName": memberArray[i].last_name,
+                            "address": memberArray[i].office,
+                            "addressSecondLine": "Washington, DC 20510",
+                            "inOffice": memberArray[i].in_office,
+                            "state": memberArray[i].state,
+                            "phone": memberArray[i].phone,
+                            "fax": memberArray[i].fax,
+                            "site": memberArray[i].url, 
+                            "email": memberArray[i].contact_form
+                        }
+                    )
+
+                    allCongressPeople.push(
+                        {
+                            "title": memberArray[i].short_title,
+                            "firstName": memberArray[i].first_name,
+                            "lastName": memberArray[i].last_name,
+                            "address": memberArray[i].office,
+                            "addressSecondLine": "Washington, DC 20510",
+                            "inOffice": memberArray[i].in_office,
+                            "state": memberArray[i].state,
+                            "phone": memberArray[i].phone,
+                            "fax": memberArray[i].fax,
+                            "site": memberArray[i].url, 
+                            "email": memberArray[i].contact_form
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
+function getHouseMembers() {
+    fetch(
+        `https://api.propublica.org/congress/v1/${congressNumber}/house/members.json`,
+        {
+            method: "GET",
+            headers: apiKeyHeader
+        }
+    ).then(
+        response => response.json()
+    ).then(
+        json => {
+            const memberArray = json.results[0].members
+            for (let i = 0; i < memberArray.length; i++) {
+                if (memberArray[i].in_office === true) {
+                    recipients.push(
+                        {
+                            "title": memberArray[i].short_title,
+                            "firstName": memberArray[i].first_name,
+                            "lastName": memberArray[i].last_name,
+                            "address": memberArray[i].office,
+                            "addressSecondLine": "Washington, DC 20515",
+                            "inOffice": memberArray[i].in_office,
+                            "state": memberArray[i].state,
+                            "phone": memberArray[i].phone,
+                            "fax": memberArray[i].fax,
+                            "site": memberArray[i].url, 
+                            "email": memberArray[i].contact_form
+                        }
+                    )
+                    houseMembers.push(
+                        {
+                            "title": memberArray[i].short_title,
+                            "firstName": memberArray[i].first_name,
+                            "lastName": memberArray[i].last_name,
+                            "address": memberArray[i].office,
+                            "addressSecondLine": "Washington, DC 20515",
+                            "inOffice": memberArray[i].in_office,
+                            "state": memberArray[i].state,
+                            "phone": memberArray[i].phone,
+                            "fax": memberArray[i].fax,
+                            "site": memberArray[i].url, 
+                            "email": memberArray[i].contact_form
+                        }
+                    )
+                    allCongressPeople.push({
                         "title": memberArray[i].short_title,
                         "firstName": memberArray[i].first_name,
                         "lastName": memberArray[i].last_name,
                         "address": memberArray[i].office,
-                        "addressSecondLine": "Washington, DC 20510"
-                    }
-                )
+                        "addressSecondLine": "Washington, DC 20515",
+                        "inOffice": memberArray[i].in_office,
+                        "state": memberArray[i].state,
+                        "phone": memberArray[i].phone,
+                        "fax": memberArray[i].fax,
+                        "site": memberArray[i].url, 
+                        "email": memberArray[i].contact_form
+                    })
+                }
             }
         }
     )
 }
 
-function getHouseMembers() {
-
-}
-
 function addToSelectionList() {
     setTimeout(
-        function() {
-            console.log('waiting...',recipients.length)
-            for (let i=0; i<recipients.length; i++) {
-                console.log(recipients[i])
+        function () {
+            // console.log('waiting...',recipients.length)
+            for (let i = 0; i < recipients.length; i++) {
+                // console.log(recipients[i])
                 reciptientOptions.options[i] = new Option(
                     `${recipients[i].title} ${recipients[i].firstName} ${recipients[i].lastName}`,
-                    `${recipients[i].title}-${recipients[i].firstName}-${recipients[i].lastName}`
+                    `${recipients[i].title} ${recipients[i].firstName} ${recipients[i].lastName}`
                 )
             }
-        }, 
+        },
         1500
     )
-    console.log('after')
 }
 
-function createLetters() {
-    console.log(recipients)
+function determineRecipientList() {
+
+    if (reciptientOptions.value.trim() === "All Senators") {
+        // console.log(senators)
+        createLetters(senators)
+    } else if (reciptientOptions.value.trim() === "All House Members") {
+        // console.log(houseMembers)
+        createLetters(houseMembers)
+    } else if (reciptientOptions.value.trim() === "All Senators and House Members") {
+        // console.log(allCongressPeople)
+        createLetters(allCongressPeople)
+    } else {
+        for (let i=0; i<allCongressPeople.length; i++) {
+            if (reciptientOptions.value.trim() === `${recipients[i].title} ${recipients[i].firstName} ${recipients[i].lastName}`) {
+                singleMember.push(
+                    recipients[i]
+                )
+                createLetters(singleMember)
+                singleMember.length = 0
+            }
+        }
+    }
 }
 
-function init () {
-    getSenateMembers()
-    // getHouseMembers()
-    addToSelectionList()
-    // createLetters()
+function createLetters(recipientArray) {
+    for (let i=0; i<recipientArray.length; i++) {
+        console.log(
+            `${recipientArray[i].title} ${recipientArray[i].firstName} ${recipientArray[i].lastName}`,
+            "\n",
+            `${recipientArray[i].address}`,
+            "\n",
+            `${recipientArray[i].addressSecondLine}`
+            )
+    }
 }
 
-init();
+generateButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    determineRecipientList();
+})
+
+getSenateMembers()
+getHouseMembers()
+addToSelectionList()
